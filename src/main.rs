bdbea6a59859e51dc1063fa664eb0e039fb4677b
Renan Vieira <>
2023-10-21 17:56:56 +0200

    Parse path from request

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::{prelude::*};
-use std::io::{Error, Write, BufReader};
+use std::io::prelude::*;
+use std::io::{BufReader, Error, Write};
 use std::net::{TcpListener, TcpStream};
 
 fn main() -> Result<(), Error> {
@@ -18,9 +18,22 @@ fn main() -> Result<(), Error> {
 fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
     let buf_reader = BufReader::new(&mut stream);
 
-    let http_request : Vec<_> = buf_reader.lines().map(|x| x.unwrap()).take_while(|x|!x.is_empty()).collect();
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|x| x.unwrap())
+        .take_while(|x| !x.is_empty())
+        .collect();
 
-    stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
+    let status_line: Vec<&str> = http_request[0].split(' ').collect();
 
+    let method = status_line[0];
+    let path = status_line[1];
+    let http_version = status_line[2];
+
+    if path == "/" {
+        stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())?;
+    } else {
+        stream.write_all("HTTP/1.1 404 Not Found\r\n\r\n".as_bytes())?;
+    }
     Ok(())
 }
