58d661bc2b27b03be5c46a838020a1b4ccf30bfd
Renan Vieira <>
2023-10-23 13:29:08 +0200

    add debug  flag

--- src/main.rs
+++ src/main.rs
@@ -31,16 +31,19 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
     let path = status_line[1];
     let http_version = status_line[2];
 
+    println!("Request: {} {}", method, path);
     let request_headers = parse_headers(&http_request);
-    let path_split: Vec<String> = path.split('/').map(|s|s.to_string()).collect();
+    let path_split: Vec<String> = path.splitn(3,'/').map(|s|s.to_string()).collect();
 
     let endpoint = path_split[1].to_owned();
     let empty_content = String::new();
     let content = path_split.get(2).unwrap_or(&empty_content);
+    let content_bytearray = content.as_bytes();
     let mut response_headers: HashMap<&str, String> = HashMap::new();
 
     response_headers.insert("Content-Type", "text/plain".to_owned());
-    response_headers.insert("Content-Length", content.as_bytes().len().to_string());
+    response_headers.insert("Content-Length",content_bytearray.len().to_string());
+    
 
     let ok_response_line = "HTTP/1.1 200 OK\r\n";
     let not_found_response_line = "HTTP/1.1 404 NotFound\r\n";
@@ -51,15 +54,20 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), Error> {
 
     match endpoint.as_ref() {
         "" => {
+            println!("Route Matched '/': {} - {:#?}", endpoint, path_split);
             stream.write_all(ok_response_line.as_bytes())?;
             stream.write_all("\r\n\r\n".as_bytes())?;
         }
         "echo" => {
+            println!("Route Matched '/echo': {} - {:#?} - ", endpoint, path_split);
             stream.write_all(ok_response_line.as_bytes())?;
             stream.write_all(headers_line.join("\r\n").as_bytes())?;
-            stream.write(content.as_bytes())?;
+            stream.write_all("\r\n\r\n".as_bytes())?;
+            stream.write_all(content_bytearray)?;
         }
         _ => {
+            println!("Not Matching Found {} - {:#?}", endpoint, path_split);
+
             stream.write_all(not_found_response_line.as_bytes())?;
             stream.write_all("\r\n".as_bytes())?;
         }
